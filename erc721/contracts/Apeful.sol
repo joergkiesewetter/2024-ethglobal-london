801ae9cbd5e33fc9d53178cecb7e9cf407b7f4a7
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2024-03-16 10:56:36 +0000

    added image generation capability to apeful NFT

--- erc721/contracts/Apeful.sol
+++ erc721/contracts/Apeful.sol
@@ -2,23 +2,106 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Apeful is ERC721, ERC721Enumerable, ERC721Burnable {
+interface IOracle {
+    function createFunctionCall(
+        uint functionCallbackId,
+        string memory functionType,
+        string memory functionInput
+    ) external returns (uint i);
+}
+
+contract Apeful is ERC721URIStorage, ERC721Enumerable, ERC721Burnable {
     uint256 private _nextTokenId;
 
-    constructor()
-        ERC721("Apeful", "AF")
-    {}
-
-    function safeMint(address to) public {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
+    struct MintInput {
+        address owner;
+        string prompt;
+        bool isMinted;
     }
 
+    mapping(uint => MintInput) public mintInputs;
+    uint private mintsCount;
+
+    event MintInputCreated(address indexed owner, uint indexed chatId);
+
+    address public oracleAddress;
+
+    string public prompt;
+
+    event PromptUpdated(string indexed newPrompt);
+    event OracleAddressUpdated(address indexed newOracleAddress);
+
+    constructor(
+        address initialOracleAddress,
+        string memory initialPrompt
+    ) ERC721("Apeful", "AF") {
+        owner = msg.sender;
+        oracleAddress = initialOracleAddress;
+        prompt = initialPrompt;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+    modifier onlyOracle() {
+        require(msg.sender == oracleAddress, "Caller is not oracle");
+        _;
+    }
+
+    function setPrompt(string memory newPrompt) public onlyOwner {
+        prompt = newPrompt;
+        emit PromptUpdated(newPrompt);
+    }
+
+    function setOracleAddress(address newOracleAddress) public onlyOwner {
+        oracleAddress = newOracleAddress;
+        emit OracleAddressUpdated(newOracleAddress);
+    }
+
+    function initializeMint(string memory message) public returns (uint i) {
+        MintInput storage mintInput = mintInputs[mintsCount];
+
+        mintInput.owner = msg.sender;
+        mintInput.prompt = message;
+        mintInput.isMinted = false;
+
+        uint currentId = mintsCount;
+        mintsCount = currentId + 1;
+
+        string memory fullPrompt = prompt;
+        fullPrompt = string.concat(fullPrompt, message);
+        fullPrompt = string.concat(fullPrompt, "\"");
+        IOracle(oracleAddress).createFunctionCall(
+            currentId,
+            "image_generation",
+            fullPrompt
+        );
+        emit MintInputCreated(msg.sender, currentId);
+
+        return currentId;
+    }
+
+    function onOracleFunctionResponse(
+        uint runId,
+        string memory response,
+        string memory errorMessage
+    ) public onlyOracle {
+        MintInput storage mintInput = mintInputs[runId];
+        require(!mintInput.isMinted, "NFT already minted");
+
+        mintInput.isMinted = true;
+
+        uint256 tokenId = _nextTokenId++;
+        _mint(mintInput.owner, tokenId);
+        _setTokenURI(tokenId, response);
+    }
     // The following functions are overrides required by Solidity.
 
     function _update(address to, uint256 tokenId, address auth)
