b9da6753f677e4efda4c2496b799db3e80c51eff
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2024-03-16 11:20:06 +0000

    adding deployment configuration to the NFT

--- erc721/ignition/modules/Apeful.ts
+++ erc721/ignition/modules/Apeful.ts
@@ -1,7 +1,10 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const TokenModule = buildModule("ApefulModule", (m) => {
-  const token = m.contract("Apeful");
+const TokenModule = buildModule("ApefulModule", (m: any) => {
+  const token = m.contract("Apeful", [
+    "0xACB8a1fcC06f1a199C1782414E39BdB4A8238e69",
+    "a comic book cover of an ape wearing a medieval crown and rope, sitting on a throne,",
+  ]);
 
   return { token };
 });
