270e11deced24d0e0c10941e37c062122c3df677
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2024-03-17 03:08:46 +0000

    WIP

---
+++ uniswap-v4/script/SetupPool.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import "../src/Apecoin.sol";
+import "../src/ApefulHook.sol";
+import "../src/PoolInitialize.sol";
+import "../src/USDC.sol";
+
+contract SetupPool is Script {
+    function setUp() public {}
+
+    function run() public {
+        
+        address deployerPublicKey = vm.envAddress("DEPLOYER_PUBLIC_KEY");
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        IPoolManager poolManager = IPoolManager(vm.envAddress("SEPOLIA_POOL_MANAGER"));
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        ApeCoin apeCoin = new ApeCoin(deployerPublicKey);
+        USDC usdc = new USDC(deployerPublicKey);
+
+        ApefulHook hook = new ApefulHook(poolManager);
+
+        PoolInitialize poolInitialize = new PoolInitialize();
+
+        bytes memory hookData = new bytes(0);
+
+        poolInitialize.init(
+            address(apeCoin),
+            address(usdc),
+            500,
+            60,
+            address(hook),
+            1909968240759429201598133539,
+            ""
+        );
+
+        vm.stopBroadcast();
+    }
+}
--- uniswap-v4/src/ApefulHook.sol
+++ uniswap-v4/src/ApefulHook.sol
@@ -1,30 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {BaseHook} from "@v4-by-example/utils/BaseHook.sol";
-
+import {BaseHook} from "./BaseHook.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {Currency} from "v4-core/types/Currency.sol";
 
 contract ApefulHook is BaseHook {
     using PoolIdLibrary for PoolKey;
 
     mapping(address user => bool allowed) public allowedUsers;
 
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+    address public hyperlaneMailboxAddress;
+    Currency public apeCoinCurrency;
+    constructor(IPoolManager _poolManager, address _hyperlaneMailbox, address _apeCoinAddress) BaseHook(_poolManager) {
+        hyperlaneMailboxAddress = _hyperlaneMailbox;
+        apeCoinCurrency = Currency.wrap(_apeCoinAddress);
+    }
 
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata hookData)
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata hookData)
         external
         override
         returns (bytes4)
     {
         // --- Read the user's address --- //
         address user = abi.decode(hookData, (address));
-        require(allowedUsers[user], "MsgSenderHookData: User not allowed");
-        return BaseHook.beforeSwap.selector;
+
+        bool giveRewards = false;
+
+        // zeroForOne and token1 == apecoin
+        if (params.zeroForOne && key.currency1 == apeCoinCurrency) {
+            giveRewards = true;
+        } else if (!params.zeroForOne && key.currency0 == apeCoinCurrency) {
+            giveRewards = true;
+        }
+
+        if (giveRewards) {
+            string message = "";
+
+            if (params.amountSpecified == 1e18) {
+                message = " with a dirty sewer background";
+            } else if (params.amountSpecified == 1337e18) {
+                message = " with a cool cyberpunk background";
+            } else if (params.amountSpecified >= 50000e18) {
+                message = " with a golden background";
+            } else if (params.amountSpecified >= 5000e18) {
+                message = " with a silver background";
+            } else if (params.amountSpecified >= 500e18) {
+                message = " with a bronze background";
+            }
+
+            
+        }
     }
 
     // Helper function for demonstration
