d8f8522eadc9e03c4c0dd25f2b8a20d6b9ac19ec
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2024-03-17 01:07:50 +0000

    intermediate commit to add a library

---
+++ uniswap-v4/script/PoolInitialize.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import "../src/ApefulHook.sol";
+import "../src/PoolInitialize.sol";
+
+contract CounterScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        NFT nft = new NFT("NFT_tutorial", "TUT", "baseUri");
+
+        vm.stopBroadcast();
+    }
+}
