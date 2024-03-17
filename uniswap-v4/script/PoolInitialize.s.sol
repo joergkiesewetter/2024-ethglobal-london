db90defe4412dcd969114c0857b3a5a46c439e87
Jörg Kiesewetter <joerg.kiesewetter@gmail.com>
2024-03-17 01:13:15 +0000

    ...

--- uniswap-v4/script/PoolInitialize.s.sol
+++ uniswap-v4/script/PoolInitialize.s.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import { Apecoin } from "../src/Apecoin.sol";
 import "../src/ApefulHook.sol";
 import "../src/PoolInitialize.sol";
 
-contract CounterScript is Script {
+contract PoolInitialize is Script {
     function setUp() public {}
 
     function run() public {
         
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerPublicKey = vm.envAddress("DEPLOYER_PUBLIC_KEY");
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        NFT nft = new NFT("NFT_tutorial", "TUT", "baseUri");
+        Apecoin Apecoin = new Apecoin(deployerPublicKey);
 
         vm.stopBroadcast();
     }
--- uniswap-v4/src/ApefulHook.sol
+++ uniswap-v4/src/ApefulHook.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.19;
 
 import {BaseHook} from "@v4-by-example/utils/BaseHook.sol";
 
-import {Hooks} from "v4-core/src/libraries/Hooks.sol";
-import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 
 contract ApefulHook is BaseHook {
     using PoolIdLibrary for PoolKey;
